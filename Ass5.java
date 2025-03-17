df7a71d02d865325d3ee10021b9e8cc83e3b5cd5
coder-raghab <raghabganguly664@gmail.com>
2025-03-17 07:19:39 +0530

    Sum of Digits of a number

--- Ass5.java
+++ Ass5.java
@@ -9,6 +9,27 @@ import java.util.Scanner;
 
 public class Ass5 {
 	public static void main(String... args) {
+		Scanner scanner = new Scanner(System.in);
+		int number;
+		int sum = 0;
+		int temp;
 		
+		System.out.print("Enter a number: ");
+		number = scanner.nextInt();
+		
+		temp = Math.abs(number);
+		
+		while(temp > 0) {
+			sum += temp % 10;
+			temp /= 10;
+		}
+		System.out.println("The sum of Digits of "+number+" is: "+sum);
+		scanner.close();
 	}
 }
+
+/*Output:
+D:\Java_Code_Practices\1_Java_Clg_Assignmetns>java Ass5.java
+Enter a number: -123
+The sum of Digits of -123 is: 6
+*/
